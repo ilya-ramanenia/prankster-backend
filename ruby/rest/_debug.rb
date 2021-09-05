ebef1d8d98c6da0a5fe98e0c89c89f33cc94f1b8
ILYA Ramanenia <ruby.royd@gmail.com>
2021-09-05 14:09:02 +0400

    Reset database debug request

--- ruby/rest/_debug.rb
+++ ruby/rest/_debug.rb
@@ -9,3 +9,11 @@ end
 get "/parent/all" do
   successResponse(200, response: Parent.all.as_json)
 end
+
+
+post "/reset_database" do
+  exec 'pg:reset --confirm prankster-app'
+  exec 'run rake db:migrate'
+
+  successResponse(200, response: "Reseted")
+end
