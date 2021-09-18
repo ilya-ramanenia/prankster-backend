55dc2cee2ba467059108d030cb784ce55485ec9b
ILYA Ramanenia <ruby.royd@gmail.com>
2021-09-18 22:25:36 +0300

    Push debug requests and fixes

--- ruby/push/push_android.rb
+++ ruby/push/push_android.rb
@@ -2,35 +2,36 @@ require 'memoist'
 
 ## Init app
 extend Memoist
-def init_android
-	androidPushApp = Rpush::Gcm::App.new
-	androidPushApp.name = "android_app"
-	androidPushApp.auth_key = "..."
-	androidPushApp.connections = 1
-	androidPushApp.save!
+def init_android_app
+	app = Rpush::Gcm::App.new
+	app.name = "android_app"
+	app.auth_key = "AAAAJNrbo5c:APA91bE-M2NGZTsnP44iPsX2NRhOpd59T-399adLmJT3UY3Tu2HTXFPHChVLLXAiW0jr42il07s3mrGjy3SHjeBqHn7J5r7SUiPlwJrqkTRe088808okxHSFy8t5p31Cn_WXFPNnn1FI"
+	app.connections = 1
+	app.save!
 end
-memoize :init_android
+memoize :init_android_app
 
 def send_silent_push_android(id, data:)
 	n = Rpush::Gcm::Notification.new
-	# n.app = Rpush::Gcm::App.find_by_name("android_app")
-	n.app = androidPushApp
+	n.app = Rpush::Gcm::App.find_by_name("android_app")
 	n.registration_ids = [id]
+	n.priority = 'high'
 	n.data = { message: "Maks soset salo" }
-	n.priority = 'high'        # Optional, can be either 'normal' or 'high'
-	n.content_available = true # Optional
+	n.priority = 'high'
+	n.content_available = true
 	n.save!
 end
 
 
 def send_message_push_android(id, title:, body:)
 	n = Rpush::Gcm::Notification.new
-	n.app = androidPushApp
+	n.app = Rpush::Gcm::App.find_by_name("android_app")
 	n.registration_ids = [id]
-	n.priority = 'high'        # Optional, can be either 'normal' or 'high'
-	# Optional notification payload. See the reference below for more keys you can use!
-	n.notification = { body: 'ЕЕЕЕ',
-		title: 'РОКККККК',
+	n.priority = 'normal'
+
+	n.notification = { 
+		body: body,
+		title: title,
 		icon: 'myicon'
 	}
 	n.save!
--- ruby/rest/_debug.rb
+++ ruby/rest/_debug.rb
@@ -11,6 +11,49 @@ get "/parent/all" do
 end
 
 
+require './ruby/push/push.rb'
+
+post "/child/send_push" do
+  auth_token = params[:auth_token].to_s
+  title = params[:title].to_s
+  body = params[:body].to_s
+
+  child = Child.find_by(auth_token: auth_token)
+  if child == nil
+      return error_response(401, error: "unauthorised")
+  end
+
+  device_info = child.device_info
+  if device_info == nil
+      return error_response(403, error: "error", debug: "no device_info in child #{child.id}")
+  end
+
+  send_message_push(device_info, title: title, body: body)
+
+  success_response(200, response: nil)
+end
+
+post "/parent/send_push" do
+  auth_token = params[:auth_token].to_s
+  title = params[:title].to_s
+  body = params[:body].to_s
+
+  parent = Parent.find_by(auth_token: auth_token)
+  if parent == nil
+      return error_response(401, error: "unauthorised")
+  end
+
+  device_info = parent.device_info
+  if device_info == nil
+      return error_response(403, error: "error", debug: "no device_info in parent #{parent.id}")
+  end
+
+  send_message_push(device_info, title: title, body: body)
+
+  success_response(200, response: nil)
+end
+
+
 post "/reset_database" do
   exec 'pg:reset --confirm prankster-app'
   exec 'run rake db:migrate'
